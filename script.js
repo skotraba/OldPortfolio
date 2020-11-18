9604c6a56fa1e77d239e5c38c4276be295010a11
Shannon <skotraba@gmail.com>
2020-11-18 13:48:00 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+
+//Show Code for J Game
+const showCode2 = document.getElementById('showCode1');
+showCode2.addEventListener('click', () => {
+    window.open('https://github.com/skotraba/GuessColor');
+});
+
+//Show Code for Color Picker
+const showCode3 = document.getElementById('showCode2');
+showCode3.addEventListener('click', () => {
+    window.open('https://github.com/skotraba/Color-Picker');
+});
+
+//Show Code for Library Management
+const showCode4 = document.getElementById('showCode3');
+showCode4.addEventListener('click', () => {
+    window.open('https://github.com/skotraba/Library-Management');
+});
+
+//Show Code for Recipe Calculator
+const showCode1 = document.getElementById('showCode4');
+showCode1.addEventListener('click', () => {
+    window.open('https://github.com/skotraba/RecipeCalculatorGUI');
+});
+
+//Show Code for Mock Website
+const showCode5 = document.getElementById('showCode5');
+showCode5.addEventListener('click', () => {
+    window.open('https://github.com/skotraba/CoffeeSite');
+});
+
+//Show Code for IT Game
+const showCode6 = document.getElementById('showCode6');
+showCode6.addEventListener('click', () => {
+    window.open('https://github.com/skotraba/ITGame');
+});
+
+//Show Code for Spanish Learning
+const showCode7 = document.getElementById('showCode7');
+showCode7.addEventListener('click', () => {
+    window.open('https://github.com/skotraba/SpanishLearn');
+});
+
+//Direct to play guess color game
+const playGuess = document.getElementById('playGame1');
+playGuess.addEventListener('click', () => {
+    location.href = './OldWork/colorGuess/index2.html';
+})
